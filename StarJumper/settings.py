748589e62e0412250bb581bb390473cbc371a819
Tim Wilson <timwilsonr@gmail.com>
2018-07-24 14:29:42 -0700

    trying to fix error 8

--- StarJumper/settings.py
+++ StarJumper/settings.py
@@ -159,8 +159,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = 'https://storage.googleapis.com/starjumper-static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+#STATIC_ROOT = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
--- StarJumper/urls.py
+++ StarJumper/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('app/', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
-
-
