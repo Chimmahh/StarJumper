bf252c3d58e78d9ae624ebb9c6b22f741e0d7643
Tim Wilson <timwilsonr@gmail.com>
2018-07-26 15:55:23 -0700

    host update via cmd!

--- StarJumper/settings.py
+++ StarJumper/settings.py
@@ -161,8 +161,7 @@ LOGIN_URL = '/app/login_register/'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# STATIC_ROOT = '/static/'
+STATIC_ROOT = '/home/codeguild/webapps/starjumper_static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
